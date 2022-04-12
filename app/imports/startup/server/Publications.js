cc6510934e2760449c1e4180dd9dc11259027601
Derrick Luyen <46693824+derrickluyen@users.noreply.github.com>
2022-04-11 22:23:36 -1000

    fixed reloading profile page bug

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -29,3 +29,7 @@ Meteor.publish(null, function () {
   }
   return this.ready();
 });
+
+Meteor.publish('viewProfile', function() {
+  return this.ready();
+});
--- app/imports/ui/pages/ViewProfile.jsx
+++ app/imports/ui/pages/ViewProfile.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Grid, Header, Card, Image } from 'semantic-ui-react';
+import { Grid, Header, Card, Image, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 
 /** Renders the Page for adding a document. */
 class ViewProfile extends React.Component {
+
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
     return (
       <Grid container centered>
         <Grid.Column>
@@ -29,13 +34,17 @@ class ViewProfile extends React.Component {
 }
 
 ViewProfile.propTypes = {
-  user: PropTypes.object.isRequired,
+  user: PropTypes.object,
+  ready: PropTypes.bool.isRequired,
 };
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
+  const subscription = Meteor.subscribe('viewProfile');
+  const ready = subscription.ready();
   const user = Meteor.user();
   return {
     user,
+    ready,
   };
 })(ViewProfile);
