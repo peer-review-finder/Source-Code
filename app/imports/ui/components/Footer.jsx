74abe8a382d6eb15ebb23bb8a3e41fb2d24e082a
Aditi Jaiswal <adi.jaiswal.svnit@gmail.com>
2022-04-14 11:58:01 -1000

    Issue #1 added UH green color to navbar and footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', marginTop: '15px', paddingBottom: '150px', color: 'white' };
     return (
-      <footer>
+      <div style={{ backgroundColor: 'rgb(33, 90, 69)' }}>
+      <footer className='no-bottom-margin'>
         <div style={divStyle} className="ui center aligned container">
           <hr />
               Department of Information and Computer Sciences <br />
@@ -14,6 +15,7 @@ class Footer extends React.Component {
           <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
         </div>
       </footer>
+        </div>
     );
   }
 }
