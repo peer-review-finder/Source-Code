5bf4d8120fe113563bd994080fdd7809f4c0530c
Your Name <yiwenc22@gmail.com>
2022-05-01 17:01:36 -1000

    test Update

--- app/tests/listReview.page.js
+++ app/tests/listReview.page.js
@@ -2,25 +2,13 @@ import { Selector } from 'testcafe';
 
 class ListReviewPage {
   constructor() {
-    this.pageId = '#list-reviews-page';
+    this.pageId = '#list-user-reviews-page';
     this.pageSelector = Selector(this.pageId);
   }
 
   /** Checks that this page is currently displayed. */
   async isDisplayed(testController) {
     await testController.expect(this.pageSelector.exists).ok();
-  }
-
-  /** check if there more than one card */
-  async hasListing(testController) {
-    await this.isDisplayed(testController);
-    await testController.expect(Selector('.ui .card').count).gte(1);
-  }
-
-  /** Go to the view paper page for the first paper */
-  async gotoViewPaperPage(testController) {
-    await this.isDisplayed(testController);
-    await testController.click('.view-paper-button');
   }
 
   // async deletePaper(testController) {
